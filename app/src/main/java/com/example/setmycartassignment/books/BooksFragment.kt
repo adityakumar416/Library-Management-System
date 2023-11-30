b3c378cf4d9ed25c6663d5ab24562c00b3d01da5
Aditya Kumar <aditya0683624@gmail.com>
2023-12-01 00:17:48 +0530

    Add Multi Language functionality in App.

--- app/src/main/java/com/example/setmycartassignment/books/BooksFragment.kt
+++ app/src/main/java/com/example/setmycartassignment/books/BooksFragment.kt
@@ -3,83 +3,111 @@ package com.example.setmycartassignment.books
 import android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.setmycartassignment.MainActivity
 import com.example.setmycartassignment.R
 import com.example.setmycartassignment.databinding.FragmentBooksBinding
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 
 class BooksFragment : Fragment() {
 
     private lateinit var binding: FragmentBooksBinding
     private lateinit var booksList: ArrayList<BooksModel>
+    private lateinit var showAllBooksAdapter: ShowAllBooksAdapter
+
+    private var lastItemId: String? = null
+    private val pageSize = 10 // Adjust the page size as needed
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        // Inflate the layout for this fragment
         binding = FragmentBooksBinding.inflate(layoutInflater, container, false)
         binding.progressBar.visibility = View.VISIBLE
 
-
         binding.addBooks.setOnClickListener {
-
             findNavController().navigate(R.id.action_booksFragment_to_addBooksFragment)
         }
 
-
         binding.recyclerview.layoutManager = LinearLayoutManager(context)
         booksList = arrayListOf()
+        showAllBooksAdapter = ShowAllBooksAdapter(booksList, requireContext())
+        binding.recyclerview.adapter = showAllBooksAdapter
 
-        (activity as MainActivity).supportActionBar?.title = "Books"
+        (activity as MainActivity).supportActionBar?.title = context?.getString(R.string.books)
 
+        loadBooks()
 
+        binding.recyclerview.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
 
-        val databaseReference = FirebaseDatabase.getInstance().getReference("books")
+                val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                val visibleItemCount = layoutManager.childCount
+                val totalItemCount = layoutManager.itemCount
+                val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+
+                if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount && firstVisibleItemPosition >= 0) {
+                    // Load the next page when the end of the list is reached
+                    loadNextPage()
+                }
+            }
+        })
+
+        return binding.root
+    }
+
+    private fun loadBooks() {
+        val databaseReference = FirebaseDatabase.getInstance().getReference("books").limitToFirst(pageSize)
         databaseReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 booksList.clear()
                 Log.i(ContentValues.TAG, "User Books $snapshot")
                 for (dataSnapshot in snapshot.children) {
-
-                    val booksModel: BooksModel? = dataSnapshot.getValue(
-                        BooksModel::class.java)
+                    val booksModel: BooksModel? = dataSnapshot.getValue(BooksModel::class.java)
                     if (booksModel != null) {
                         binding.progressBar.visibility = View.GONE
-
                         booksList.add(booksModel)
+                        lastItemId = dataSnapshot.key // Track the last item id
                     }
-
                 }
-
-
-                binding.recyclerview.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL,false)
-
-                binding.recyclerview.adapter = ShowAllBooksAdapter(booksList)
+                showAllBooksAdapter.notifyDataSetChanged()
             }
 
             override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(context,error.toString(), Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show()
             }
-
-
         })
-
-
-
-        return binding.root
     }
 
+    private fun loadNextPage() {
+        val databaseReference = FirebaseDatabase.getInstance().getReference("books")
+            .startAt(lastItemId) // Start after the last item in the current page
+            .limitToFirst(pageSize)
+
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (dataSnapshot in snapshot.children) {
+                    val booksModel: BooksModel? = dataSnapshot.getValue(BooksModel::class.java)
+                    if (booksModel != null) {
+                        booksList.add(booksModel)
+                        lastItemId = dataSnapshot.key // Track the last item id
+                    }
+                }
+                showAllBooksAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
 }
